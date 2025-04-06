8c97e63d4ec3169d7b11162fe822f9ef929c663f
Bojan-pk <dordevicbojan@gmail.com>
2025-04-06 07:56:39 +0200

    sitno nesto

--- resources/views/livewire/user-logins.blade.php
+++ resources/views/livewire/user-logins.blade.php
@@ -1,6 +1,6 @@
 
 
-<form wire:submit="submitForm" class=" flex justify-center">
+<div  class=" flex justify-center">
     <div class="w-10/12 rounded border p-2">
 
         <x-flash-message/>
@@ -49,58 +49,6 @@
                 class="block w-full p-2.5 ps-10 text-sm text-gray-500 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Претражи ...." />
         </div>
-        {{-- <div>
-            <x-input-text name="form.name" label="Име"/>
-            <x-input-text name="form.email" label="Е-маил"/>
-            <x-input-text name="form.password" label="Лозинка"/>
-            
-            <x-input-select name="form.role" label="Улога" :options="$roles"
-                    optionValue="value" :optionText="['name']" />
+        
+</div
 
-            
-            <div class="flex justify-between">
-                <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" type="button"
-                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-4">Обриши корисника</button>
-                <button wire:click="cleanTable()" type="button"
-                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-4">Обриши
-                    унос</button>
-                <button type="submit"
-                    class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none mt-4">Унеси</button>
-            </div>
-
-        </div> --}}
-</form>
-<div id="popup-modal" tabindex="-1"
-    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        {{-- <div class="relative bg-white rounded-lg shadow ">
-            <button type="button"
-                class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
-                data-modal-hide="popup-modal">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <div class="p-4 md:p-5 text-center">
-                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 " aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-                <h3 class="mb-5 text-lg font-normal text-gray-500 ">Да ли сте сигурни да желите да
-                    обришите корисника?</h3>
-                <button data-modal-hide="popup-modal" type="button"
-                    wire:click="removeUser({{ $form->id }})"
-                    class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300  font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                    Да, јесам
-                </button>
-                <button data-modal-hide="popup-modal" type="button"
-                    class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Не,
-                    откажи</button>
-            </div>
-        </div> --}}
-    </div>
-</div>
